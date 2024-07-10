e8b59085a8ae38a663107c58a8e165e1db8def04
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2024-07-10 15:43:44 +0300

    Новая, уникальная, увлекательная игра эксклюзивно для ExtremeCodeOS - NumberGuesser.

---
+++ src/Games/NumberGuesser/NumberGuesser.cs
@@ -0,0 +1,6 @@
+public class NoGuess()
+{
+  Console.WriteLine("Загадай число и напиши его сюда.");
+  string numberguessedlegit = Console.ReadLine();
+  Console.WriteLine("Ты загадал число: " + numberguessedlegit);
+}
